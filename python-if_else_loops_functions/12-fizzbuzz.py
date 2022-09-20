e31bfae08fae486d9132b6d029945d7efc9a4ce0
alejandrodgz <5199@holbertonschool.com>
2022-09-20 15:15:51 -0500

    task 12 v2

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-from re import I
-
-
 def fizzbuzz():
-    i = 1
-    while i < 101:
-        if i % 3 == 0 and i % 5 == 0:
-            print("{}".format("FizzBuzz"), end=" ")
-            i += 1
-        elif i % 5 == 0:
-            print("{}".format("Buzz"), end=" ")
-            i += 1
-        elif i % 3 == 0:
-            print("{}".format("Fizz"), end=" ")
-            i += 1
-        else:
-            print("{}".format(i), end=" ")
-    print("")
+        i = 1
+        while i < 101:
+                if i % 3 == 0 and i % 5 == 0:
+                        print("{}".format("FizzBuzz"), end=" ")
+                        i += 1
+                elif i % 5 == 0:
+                        print("{}".format("Buzz"), end=" ")
+                        i += 1
+                elif i % 3 == 0:
+                        print("{}".format("Fizz"), end=" ")
+                        i += 1
+                else:
+                        print("{}".format(i), end=" ")
+                        i += 1
