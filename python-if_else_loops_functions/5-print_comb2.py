551b1aeae9b5a408a51560a8d6e6c848ff0ff0db
alejandrodgz <5199@holbertonschool.com>
2022-09-20 14:07:25 -0500

    task 5 v2

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,4 @@ for a in range(0, 100):
     if a == 99:
         print("{0}".format(a))
     else:
-        if a < 10:
-            print("{0}{1}{2} ".format(a // 10, a, ","), end="")
-        else:
-            print("{0}{1} ".format(a, ","), end="")
+        print("{:02d}{} ".format(a, ","), end="")
