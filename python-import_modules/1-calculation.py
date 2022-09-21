dd42c4dab56faa065888ebdff8846013fc2ecb3e
alejandrodgz <5199@holbertonschool.com>
2022-09-21 12:15:03 -0500

    task 2 v1

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import calculator_1 as cal
+if __name__ == "__main__":
+        a = 10
+        b = 5
+        print(f"{a} + {b} = {cal.add(a, b)}")
+        print(f"{a} - {b} = {cal.sub(a, b)}")
+        print(f"{a} * {b} = {cal.mul(a, b)}")
+        print(f"{a} / {b} = {cal.div(a, b)}")
