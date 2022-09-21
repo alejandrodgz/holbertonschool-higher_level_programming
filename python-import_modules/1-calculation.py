d98c5ce9c7205f164e545fdb0a224aa2add44c44
alejandrodgz <5199@holbertonschool.com>
2022-09-21 12:17:51 -0500

    task 2 v1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -3,7 +3,7 @@ import calculator_1 as cal
 if __name__ == "__main__":
         a = 10
         b = 5
-        print(f"{a} + {b} = {cal.add(a, b)}")
-        print(f"{a} - {b} = {cal.sub(a, b)}")
-        print(f"{a} * {b} = {cal.mul(a, b)}")
-        print(f"{a} / {b} = {cal.div(a, b)}")
+        print("{} + {} = {}".format(a, b, cal.add(a, b)))
+        print("{} - {} = {}".format(a, b, cal.sub(a, b)))
+        print("{} * {} = {}".format(a, b, cal.mul(a, b)))
+        print("{} / {} = {}".format(a, b, cal.div(a, b)))
