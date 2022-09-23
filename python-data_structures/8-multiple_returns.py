fe68c97adb8cd4409de591393c00c04dc9d8f145
alejandrodgz <alejandrodgz@github.com>
2022-09-23 13:58:03 -0500

    task 8 second version

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if len(sentence) == 0:
+        tuple3 = (0, "None")
+        return tuple3
     tuple1 = (len(sentence), sentence[0])
     return tuple1
