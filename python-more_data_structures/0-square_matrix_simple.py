11cd18b2371d318e6f0d7bad03eea5a731513e56
alejandrodgz <alejandrodgz@github.com>
2022-09-26 15:09:00 -0500

    task 0

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    matrix2 = [[0 for i in range(len(matrix))] for j in range(len(matrix[0]))]
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            matrix2[i][j] = matrix[i][j] ** 2
+    return matrix2
