4befbbaaa0a30bcb41159df11855ec4b821c0638
alejandrodgz <alejandrodgz@github.com>
2022-09-26 18:01:24 -0500

    task 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if len(a_dictionary) == 0:
+        return None
+    best_score = 0
+    save = ['nothing']
+    d = {}
+    for i, j in a_dictionary.items():
+        if j > best_score:
+            best_score = j
+            save[0] = i
+    return save[0]
