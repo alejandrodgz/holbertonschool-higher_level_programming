461686807c1c81c6a40db3ad4bb5c8be5a6c3c90
alejandrodgz <alejandrodgz@github.com>
2022-09-27 11:01:47 -0500

    task 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+from re import T
+
+
 def roman_to_int(roman_string):
+    if roman_string is None:
+        return 0
+    if isinstance(roman_string, str) is False:
+        return 0
     dic = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 50}
     a = 0
     for i in range(len(roman_string) - 1):
