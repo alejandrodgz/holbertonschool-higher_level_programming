0ffe80747acd4d12c5ef30ff4ee0f019fb757082
alejandrodgz <alejandrodgz@github.com>
2022-09-27 22:38:13 -0500

    task 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,7 +2,7 @@
 def safe_print_division(a, b):
     try:
         c = a / b
-    except:
+    except BaseException:
         return None
     finally:
         if b == 0:
@@ -10,13 +10,3 @@ def safe_print_division(a, b):
         else:
             print("Inside result: {}".format(a / b))
     return c
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
