431bd7ab4b8d8da7730bfa1b8edd3ae3ae4fbc93
alejandrodgz <alejandrodgz@github.com>
2022-09-28 20:11:21 -0500

    task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+Module square
+creating a class with one attribute Size
+'''
+
+
+class Square:
+    ''' class with one attribute
+    '''
+
+    def __init__(self, size):
+        '''new attribute
+        args:
+            size: size of Square'''
+
+        self.__size = size
