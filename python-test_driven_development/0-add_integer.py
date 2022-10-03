1cf05e2c8b69d47bb7a806b0f490ddeda9c57d82
alejandrodgz <alejandrodgz@github.com>
2022-10-03 17:39:25 -0500

    task 0 v7

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -10,10 +10,10 @@ def add_integer(a, b=98):
     '''return the addition of a, b an exact integer
     no string permited
     '''
-    if not isinstance(a, int) and not isinstance(a, float):
+    if type(a) is None or(type(a) is not int and type(a) is not float):
         raise TypeError("a must be an integer")
 
-    elif not isinstance(b, int) and not isinstance(b, float):
+    elif type(b) is not int and type(b) is not float:
         raise TypeError("b must be an integer")
 
     elif a + b == float('inf') or a + b == -float('inf'):
