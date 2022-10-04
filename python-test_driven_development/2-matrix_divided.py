e9459e2836ea64c923f292386e579c1eebe3ea18
alejandrodgz <alejandrodgz@github.com>
2022-10-04 13:00:24 -0500

    task 1

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,12 +4,13 @@ element in a matrix with integers or float
 div must be different from 0
 '''
 
+
 def matrix_divided(matrix, div):
     '''matrix is a matrix with same length in its rows
     div must be different from 0
     matrix elements must be int or float type'''
 
-    if type(div) != int and type(div) != float:
+    if not isinstance(div, int) and not isinstance(div, float):
         raise TypeError("div must be a number")
 
     if div == 0:
@@ -20,15 +21,17 @@ def matrix_divided(matrix, div):
         if len(a) != b:
             raise TypeError("Each row of the matrix must have the same size")
 
-    new_matrix = [[0 for k in range(len(matrix[0]))] for l in range(len(matrix))]
+    new_matrix = [[0 for k in range(len(matrix[0]))]
+                  for l in range(len(matrix))]
     y = 0
     for i in matrix:
         z = 0
         for j in i:
-            if type(j) != int and type(j) != float:
-                raise TypeError ("matrix must be a matrix (list of lists) of integers/floats")
+            if not isinstance(j, int) and not isinstance(j, float):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
             else:
-                new_matrix[y][z] = round(float(j/div), 2)
+                new_matrix[y][z] = round(float(j / div), 2)
                 z += 1
         y += 1
     return new_matrix
