32be2901b9546b7d3859b39cfffffd127ae7ed27
alejandrodgz <alejandrodgz@github.com>
2022-10-03 20:50:39 -0500

    task 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -13,7 +13,7 @@ def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
 
-    if isinstance(a, None):
+    if a is None:
         raise TypeError("a must be an integer")
 
     elif not isinstance(b, int) and not isinstance(b, float):
