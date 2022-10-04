53bbb0c149ac4cce31edf4ad9c4af93a6b466e2d
alejandrodgz <alejandrodgz@github.com>
2022-10-04 12:48:38 -0500

    task1

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+''' module 2-matrix_divided - divided every
+element in a matrix with integers or float
+div must be different from 0
+'''
+
+def matrix_divided(matrix, div):
+    '''matrix is a matrix with same length in its rows
+    div must be different from 0
+    matrix elements must be int or float type'''
+
+    if type(div) != int and type(div) != float:
+        raise TypeError("div must be a number")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+
+    b = len(matrix[0])
+    for a in matrix:
+        if len(a) != b:
+            raise TypeError("Each row of the matrix must have the same size")
+
+    new_matrix = [[0 for k in range(len(matrix[0]))] for l in range(len(matrix))]
+    y = 0
+    for i in matrix:
+        z = 0
+        for j in i:
+            if type(j) != int and type(j) != float:
+                raise TypeError ("matrix must be a matrix (list of lists) of integers/floats")
+            else:
+                new_matrix[y][z] = round(float(j/div), 2)
+                z += 1
+        y += 1
+    return new_matrix
