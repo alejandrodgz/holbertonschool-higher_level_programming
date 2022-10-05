2d9e0e2187d017d7f6b9e6c32dac19c18f6ed413
alejandrodgz <alejandrodgz@github.com>
2022-10-05 14:03:52 -0500

    task 6

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,10 +8,30 @@ max_integer =__import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     
     def test_max(self):
-        '''test when the list have integers'''
+        '''test when the list have integers and max at the end'''
 
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4)
 
         '''test when len of list = 0'''
 
         self.assertAlmostEqual(max_integer(), None)
+
+        '''test when the list have integers and max at the start'''
+
+        self.assertAlmostEqual(max_integer([4, 2, 3, 1]), 4)
+
+        '''test when the list have integers and max at the middle'''
+
+        self.assertAlmostEqual(max_integer([4, 2, 5, 1, 3]), 5)
+
+        '''test when the list have integers and one negative number'''
+
+        self.assertAlmostEqual(max_integer([4, -2, 5, 1, 3]), 5)
+
+        '''test when the list just have one number'''
+
+        self.assertAlmostEqual(max_integer([5]), 5)
+
+        '''test when the list just have one number'''
+
+        self.assertAlmostEqual(max_integer([-4, -2, -5, -1, -3]), -1)
