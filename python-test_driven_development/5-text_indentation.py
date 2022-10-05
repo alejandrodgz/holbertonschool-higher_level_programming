0566d407ea1e77a335a316a937f7ad4153ceb812
alejandrodgz <alejandrodgz@github.com>
2022-10-04 20:36:46 -0500

    task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''this the 5-text_indentation module with one function
+called text_indentation, the function creates a new line
+when these characteres are found (. , ? :)'''
+
+
+def text_indentation(text):
+    if isinstance(text, str) is False:
+        raise TypeError('text must be a string')
+    a = 0
+    for i in range(len(text)):
+        if (text[i] == '.') or (text[i] == '?' or text[i] == ':'):
+            new_string = text[a:i + 1]
+            print(new_string)
+            print("")
+            a = i + 2
