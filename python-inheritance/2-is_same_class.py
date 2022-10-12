751588ec3253f47c3f7d14f21a3ac408faad6af1
alejandrodgz <alejandrodgz@github.com>
2022-10-11 22:43:58 -0500

    task2

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,4 +8,7 @@ def is_same_class(obj, a_class):
     '''object to analize
     class to watch
     '''
-    return isinstance(obj, a_class)
+    if isinstance(obj, a_class) and isinstance(obj, a_class) is True:
+        return True
+    else:
+        return False
