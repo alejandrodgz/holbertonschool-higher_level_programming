f3f8e4666a9a3769a5eb93ae83e434a55d251fdf
alejandrodgz <alejandrodgz@github.com>
2022-10-11 21:34:08 -0500

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -4,11 +4,15 @@ documentation for module my_list
 with Class Mylist
 """
 
+
 class Mylist(list):
     """list of int type that
     inherits from list"""
     pass
+
     def print_sorted(self):
+        """method to print every
+        element of self list sorted"""
         a = self[:]
         new_list = sorted(a)
         print(new_list)
