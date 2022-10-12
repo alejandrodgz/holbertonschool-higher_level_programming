d6bcc2b0e993df3cdbb49283af89623544372a7a
alejandrodgz <alejandrodgz@github.com>
2022-10-12 00:45:20 -0500

    task 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -3,8 +3,9 @@
 it checks if something is an instance
 of the class'''
 
+
 def inherits_from(obj, a_class):
     '''object to analize
     class to watch
     '''
-    return type(obj) is not a_class and isinstance(obj, a_class)
+    return not isinstance(obj, a_class) and isinstance(obj, a_class)
