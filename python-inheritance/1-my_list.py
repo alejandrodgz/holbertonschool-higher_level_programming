44347b2d1e0716355df74b212af203ecf5777793
alejandrodgz <alejandrodgz@github.com>
2022-10-11 21:51:15 -0500

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 """
-Module 1-my_list+
-
+Module 1-my_list
 Contains class MyList
 inherits from list; has public instance method to print sorted
 """
 
-class Mylist(list):
-    """inherits from list
 
+class MyList(list):
+    """inherits from list
     methods:
     print_sorted(self)
     """
-
     def print_sorted(self):
         """prints list of ints all sorted in ascending order"""
-        a = self[:]
-        new_list = sorted(a)
-        print(new_list)
+        print(sorted(self))
