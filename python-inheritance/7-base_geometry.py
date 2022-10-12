b6d4d970a705e878db0a390f05251417f6108612
alejandrodgz <alejandrodgz@github.com>
2022-10-12 01:54:57 -0500

    task 7

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" module 7-base_geometry
+with class BaseGeometry
+an area method"""
+
+
+from cmath import isnan
+
+
+class BaseGeometry:
+    """this is an empty class"""
+
+    def integer_validator(self, name, value):
+        """validator of integer method
+        name: name of the variable
+        value: value related to name"""
+        
+        if isinstance(value, int) is False:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{}  must be greater than 0".format(name))
+        else:
+            self.value = value
+
+
+    def area(self):
+        """method not implemented"""
+
+        raise Exception("area() is not implemented")
