9f7dc9dc82e297d03494e16283913dc87939ae96
alejandrodgz <alejandrodgz@github.com>
2022-10-11 22:49:17 -0500

    task3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''module 3-is_kind_of_class
+it checks if something is an instance
+of the class'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''object to analize
+    class to watch
+    '''
+    return isinstance(obj, a_class)
