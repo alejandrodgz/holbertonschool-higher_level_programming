d549eedd908997b2fc577e8fcc627b337fb0788a
alejandrodgz <alejandrodgz@github.com>
2022-10-12 01:12:05 -0500

    task6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" module 6-base_geometry
+with class BaseGeometry
+an area method"""
+
+
+class BaseGeometry:
+    """this is an empty class"""
+
+    def area(self):
+        """method not implemented"""
+
+        raise Exception("area() is not implemented")
