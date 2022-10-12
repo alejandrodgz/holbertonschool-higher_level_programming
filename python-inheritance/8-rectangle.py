f00daac1bf9620d2766de56a0e43d853b67e87cd
alejandrodgz <alejandrodgz@github.com>
2022-10-12 02:31:02 -0500

    task8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -25,24 +25,14 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
 class Rectangle(BaseGeometry):
+    '''new class
+    '''
+
     def __init__(self, width, height):
-        super().integer_validator('width', width)
+        '''new method'''
+
+        self.integer_validator('width', width)
         self.__width = width
-        super().integer_validator('height', height)
+        
+        self.integer_validator('height', height)
         self.__height= height
-
-
-r = Rectangle(3, 5)
-
-print(r)
-print(dir(r))
-
-try:
-    print("Rectangle: {} - {}".format(r.width, r.height))
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
-
-try:
-    r2 = Rectangle(4, True)
-except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, e))
