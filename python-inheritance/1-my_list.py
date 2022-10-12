0d783d4a1575782705ab5bf286d924cdf7c61eb9
alejandrodgz <alejandrodgz@github.com>
2022-10-11 21:37:54 -0500

    task1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""
+'''
 documentation for module my_list
 with Class Mylist but it is not 
 working
-"""
+'''
 
 class Mylist(list):
-    """list of int type that
+    '''list of int type that
     inherits from list
     list: integer elements
-    """
+    '''
+
     pass
 
     def print_sorted(self):
-        """method to print every
-        element of self list sorted"""
+        '''method to print every
+        element of self list sorted'''
         a = self[:]
         new_list = sorted(a)
         print(new_list)
