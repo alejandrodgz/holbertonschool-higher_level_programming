9fb7c1b4056816ff0dab2b6f2451d9691c5b8a6f
alejandrodgz <alejandrodgz@github.com>
2022-10-12 20:12:14 -0500

    task 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""this module has a function to read
+a file called read_file"""
+
+def read_file(filename=""):
+    """filename is the name of
+    the file to read and print"""
+
+    with open(filename, mode="r", encoding="utf-8")as f:
+        file_read=f.read()
+        print(file_read)
