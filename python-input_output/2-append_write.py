189ff8c79cc3cbdfe9c58537e7d4643da2b4d921
alejandrodgz <alejandrodgz@github.com>
2022-10-12 21:17:23 -0500

    task2

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''this module has the write_file function
+and is about writing a string to a file
+or created if does not exist'''
+
+
+def append_write(filename="", text=""):
+    """text, string
+    filename, file to write or create"""
+
+    with open(filename, mode="a+", encoding="utf-8")as f:
+        return f.write(text)
