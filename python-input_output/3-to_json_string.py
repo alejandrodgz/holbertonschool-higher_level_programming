b459002a42d2332a9f8f86ac021f35f7d9002cc9
alejandrodgz <alejandrodgz@github.com>
2022-10-12 21:29:24 -0500

    task3

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+'''module with to_json_string function
+returns the json represantation of an object'''
+
+import json
+
+def to_json_string(my_obj):
+    '''my_obj is the object
+    to convert in json representation'''
+
+    return json.dumps(my_obj)
