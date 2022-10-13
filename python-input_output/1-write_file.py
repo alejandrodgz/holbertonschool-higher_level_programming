a9bc5dffcfbe36900f5c86171da79a4d9ed6cfb9
alejandrodgz <alejandrodgz@github.com>
2022-10-12 21:12:13 -0500

    task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''this module has the write_file function
+and is about writing a string to a file
+or created if does not exist'''
+
+
+def write_file(filename="", text=""):
+    with open(filename, mode="w+", encoding="utf-8")as f:
+        return f.write(text)
