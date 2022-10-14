e816ade308542ed80b600bb3c8190a90c1282b39
alejandrodgz <alejandrodgz@github.com>
2022-10-14 11:40:42 -0500

    task4

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 '''this module has the function
-from_json_string Write a function 
+from_json_string Write a function
 that returns an object '''
 import json
+
 
 def from_json_string(my_str):
     '''my_str is the string to convert
