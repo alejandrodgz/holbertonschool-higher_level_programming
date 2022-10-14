3c9f4c3f68925161b279b171dd32b2491a8ac345
alejandrodgz <alejandrodgz@github.com>
2022-10-14 12:32:19 -0500

    task6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -4,10 +4,11 @@ function that converts json to object"""
 
 import json
 
+
 def load_from_json_file(filename):
     '''filename where we have to extract the json string
     to convert to object'''
 
-    with open(filename, mode="r+", encoding="utf-8")as f:
-        read_file=f.read()
-        return json.loads(read_file) 
+    with open(filename, mode="r", encoding="utf-8")as f:
+        read_file = f.read()
+        return json.loads(read_file)
