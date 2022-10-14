2c32ecaa4641816272b007a25022db10037892fd
alejandrodgz <alejandrodgz@github.com>
2022-10-14 15:58:31 -0500

    task11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""class student a method
+to jsonaize """
+
+
+class Student:
+    """class student bro
+    no need to know anymore"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''json method'''
+        if attrs is not None:
+            dict1 = self.__dict__
+            dict2 = {}
+            for i in dict1.keys():
+                if i in attrs:
+                    dict2[i] = dict1[i]
+            return dict2
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        self.first_name = json["first_name"]
+        self.last_name = json["last_name"]
+        self.age = json["age"]
