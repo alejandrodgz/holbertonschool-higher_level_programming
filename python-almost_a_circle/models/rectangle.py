c4910954a29cd790fb075a32faf3a7c2110e2627
alejandrodgz <alejandrodgz@github.com>
2022-10-17 16:14:56 -0500

    task1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,19 +2,7 @@
 '''this module contains a class
 called base'''
 
-
-class Base:
-    '''class private atribute
-    called __nb of objects'''
-
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        if id is None:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
-        else:
-            self.id = id
+from base import Base as Base
 
 
 class Rectangle(Base):
