b5d82de32288c4911f0bc4abb2f8e09d025aec4f
alejandrodgz <alejandrodgz@github.com>
2022-10-17 16:04:37 -0500

    task1

---
+++ python-almost_a_circle/models/Rectangle.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+'''this module contains a class
+called base'''
+
+
+class Base:
+    '''class private atribute
+    called __nb of objects'''
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is None:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+        else:
+            self.id = id
+
+
+class Rectangle(Base):
+    '''class inherited from Base
+    called Rectangle that stores, ahmm..
+    a rectangle'''
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id):
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
