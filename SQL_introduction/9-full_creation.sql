641320baa492037cf72e91ac72cf91647b842c1a
alejandrodgz <alejandrodgz@github.com>
2022-11-11 12:39:42 -0500

    task9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
 -- second table creation
 -- also data insertion 
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT)
+CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table(id, name, score)
 VALUES(1, "John", 10),
       (2, "Alex", 3),
