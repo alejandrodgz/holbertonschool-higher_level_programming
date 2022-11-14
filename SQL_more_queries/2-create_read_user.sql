86aeedc1545bfe613ebdd8e36eb896109be047ed
alejandrodgz <alejandrodgz@github.com>
2022-11-14 13:57:03 -0500

    task 2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- create database and user
+-- concede some privileges
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+GRANT SELECT
+ON hbtn_0d_2.*
+TO user_0d_2@localhost;
