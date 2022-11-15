25f0a01fdac6b7b4c7e1ac2369a20b914ce1b10b
alejandrodgz <alejandrodgz@github.com>
2022-11-14 20:23:04 -0500

    task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,5 @@
+-- display information beetween two tables
+-- tv data base
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres, tv_genres 
+WHERE tv_shows.id = tv_show_genres.show_id AND tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id;
