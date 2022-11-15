86e04495c14a6049e51ce3f11ed68ced7a782962
alejandrodgz <alejandrodgz@github.com>
2022-11-14 22:35:41 -0500

    task 14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- genres of show dexter
 -- uses of different joins
+USE hbtn_0d_tvshows;
 SELECT tv_genres.name FROM tv_genres 
 INNER JOIN tv_show_genres INNER JOIN tv_shows 
 WHERE tv_genres.id = tv_show_genres.genre_id AND show_id = 8 AND tv_shows.title = "Dexter"
