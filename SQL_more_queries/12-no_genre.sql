b163e48ca9dcf9ee79a9cc6a20a684b8a6c57dd5
alejandrodgz <alejandrodgz@github.com>
2022-11-14 21:48:26 -0500

    task 12

---
+++ SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,5 @@
+-- genre id 
+-- inner join
+SELECT tv_shows.title, tv_show_genres.genre_id FROM
+tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = show_id WHERE genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
