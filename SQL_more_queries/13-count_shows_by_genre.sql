a0f163bc53576e6964319ced25d25ea45e11ca31
alejandrodgz <alejandrodgz@github.com>
2022-11-14 22:34:12 -0500

    task 14

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,6 @@
 -- select and group
 -- join tables
 SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows 
-FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id 
+FROM tv_genres INNER JOIN tv_show_genres INNER JOIN tv_shows
+WHERE tv_genres.id = tv_show_genres.genre_id AND tv_show_genres.show_id = 8 AND tv_shows.name ="Dexter"
 GROUP BY genre_id ORDER  BY number_of_shows DESC;
--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- genres of show dexter
 -- uses of different joins
 SELECT tv_genres.name FROM tv_genres 
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id AND show_id = 8
+INNER JOIN tv_show_genres INNER JOIN tv_shows 
+WHERE tv_genres.id = tv_show_genres.genre_id AND show_id = 8 AND tv_shows.title = "Dexter"
 ORDER BY tv_genres.name ASC; 
