21b0dd3137b278adb3d322fa9f19b7fcf1e1c940
alejandrodgz <alejandrodgz@github.com>
2022-11-14 23:15:37 -0500

    task 15

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,6 @@
+-- joins in different levels
+-- select and order by
+SELECT tv_shows.title FROM tv_shows INNER JOIN tv_show_genres 
+ON tv_shows.id = tv_show_genres.show_id INNER JOIN tv_genres ON
+tv_show_genres.genre_id = tv_genres.id where tv_genres.name= "Comedy"
+ORDER BY tv_shows.title ASC;
