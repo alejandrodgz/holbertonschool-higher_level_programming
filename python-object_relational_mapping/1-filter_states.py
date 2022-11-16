758deff3b982fd006d0b6958666320620d960b91
alejandrodgz <alejandrodgz@github.com>
2022-11-15 20:16:52 -0500

    task 1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""new project python
+connect with sql server
+mysql commands"""
+import MySQLdb
+import sys
+
+if "__main__" == __name__:
+
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        database=sys.argv[3],
+        user='root',
+        password='')
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY states.id ASC;")
+    states = cur.fetchall()
+    for i in states:
+        print(i)
+    cur.close()
+    db.close()
