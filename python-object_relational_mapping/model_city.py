dea0df1159c46e59a65e0bfe645c31bcc6044b97
alejandrodgz <alejandrodgz@github.com>
2022-11-17 13:54:37 -0500

    task 14

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -5,12 +5,13 @@ inherits from Base as State do"""
 import model_state
 from sqlalchemy import Column, Integer, String, ForeignKey
 
+
 class City(model_state.Base):
     '''new class my friend'''
-    
+
     __tablename__ = "cities"
     id = Column("id", Integer, nullable=False, primary_key=True,
-         autoincrement = "auto", unique=True)
+                autoincrement="auto", unique=True)
     name = Column("name", String(128), nullable=False)
     state_id = Column("state_id",
-              Integer, ForeignKey("states.id"), nullable=False)
+                      Integer, ForeignKey("states.id"), nullable=False)
