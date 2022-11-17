104924e01ea905ac7b271819fdf098ee32829831
alejandrodgz <alejandrodgz@github.com>
2022-11-17 00:48:06 -0500

    task 13

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""retrieving info from sqalchemy
+no idea how"""
+
+
+from model_state import Base, State
+import sys
+from sqlalchemy import (create_engine, update)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    Session = Session()
+    usr_update = Session.query(State).filter(State.name.like('%a%'))
+    for i in usr_update:
+        Session.delete(i)
+    Session.commit()
+    Session.close()
