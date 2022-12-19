c0f048dea19539303f3c17d0dc5e65403c86feb2
alejandrodgz <5199@holbertonstudents.com>
2022-12-19 16:31:23 -0500

    task 7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+require('process');
+if (Number(process.argv[2])) {
+  for (let i = 0; i < Number(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
