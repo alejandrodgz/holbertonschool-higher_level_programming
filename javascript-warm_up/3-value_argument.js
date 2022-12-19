38b550de2064203a90b06d106e0ebdf7dcca01b6
alejandrodgz <5199@holbertonstudents.com>
2022-12-19 15:53:19 -0500

    task3

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -2,8 +2,8 @@
 
 require('process');
 
-if (process.argv.length >= 2) {
+if (process.argv[2]) {
   console.log(process.argv[2]);
-} else if (process.argv.length < 2) {
+} else {
   console.log('No argument');
 }
