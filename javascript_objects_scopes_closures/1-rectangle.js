ef3529b3fa0872564a53c0372e07f691630b06c4
alejandrodgz <5199@holbertonstudents.com>
2022-12-20 14:44:52 -0500

    task 0

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangule {
+    width;
+    height;
+      constructor(w, h) {
+        w = this.width;
+        h = this.height;
+      }
+  }
+  
+  const r1 = new Rectangle(2, 3);
+  console.log(r1);
+  console.log(r1.width);
+  console.log(r1.height);
