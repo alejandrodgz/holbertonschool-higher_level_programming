a70b7505de1c66007f6858d727b0b6c4e7548944
alejandrodgz <5199@holbertonstudents.com>
2022-12-20 15:47:08 -0500

    task 1

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -1,15 +1,13 @@
 #!/usr/bin/node
 
-class Rectangule {
-    width;
-    height;
-      constructor(w, h) {
-        w = this.width;
-        h = this.height;
-      }
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
-  
-  const r1 = new Rectangle(2, 3);
-  console.log(r1);
-  console.log(r1.width);
-  console.log(r1.height);
+}
+
+const r1 = new Rectangle(2, 3);
+console.log(r1);
+console.log(r1.width);
+console.log(r1.height);
