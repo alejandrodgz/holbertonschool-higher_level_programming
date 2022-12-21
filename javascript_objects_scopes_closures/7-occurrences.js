b258b67ee4f53dd07420d8125b42ddf6a973c6d0
alejandrodgz <5199@holbertonstudents.com>
2022-12-20 19:21:45 -0500

    task 11

---
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+  let a = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      a += 1;
+    }
+  }
+  return a;
+};
