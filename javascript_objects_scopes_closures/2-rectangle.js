ae2059bf097fd7eeb7254f89735aae10592f6938
alejandrodgz <5199@holbertonstudents.com>
2022-12-20 20:19:13 -0500

    task 2

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -1,16 +1,10 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
-    }
-
-    function print() {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width))
-      }
     }
   }
 }
